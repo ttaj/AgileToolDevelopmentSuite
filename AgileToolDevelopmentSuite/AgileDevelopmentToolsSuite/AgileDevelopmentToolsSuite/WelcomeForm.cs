4e340e1080d309aceb2316589f53dce393d6caa8
Nathan Tisdale-Dollah <nathantisdaledollah@yahoo.com>
2016-10-07 09:02:47 -0500

    Changes
    most recent changes from Jon and Tino's, inserting SQL and checks after
    this

---
+++ AgileToolDevelopmentSuite/AgileDevelopmentToolsSuite/AgileDevelopmentToolsSuite/WelcomeForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgileDevelopmentToolsSuite
+{
+    public partial class WelcomeForm : Form
+    {
+        public WelcomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+
+            loginForm.Width = this.Width;
+            loginForm.Height = this.Height;
+
+            loginForm.StartPosition = FormStartPosition.Manual;
+            loginForm.Location = new Point(this.Location.X, this.Location.Y);
+
+            this.Hide();
+            loginForm.Show();
+        }
+
+        private void WelcomeForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void WelcomeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void welcomeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

9b3ba5b712daa1344204be0480eb3034f047cac7
Jon <Jon@TRANDAMERE>
2016-11-27 02:57:54 -0600

    forgot to make it launch from welcomeform

--- AgileDevelopmentToolsSuite/AgileDevelopmentToolsSuite/Program.cs
+++ AgileDevelopmentToolsSuite/AgileDevelopmentToolsSuite/Program.cs
@@ -16,8 +16,8 @@ namespace AgileDevelopmentToolsSuite
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-      //Application.Run(new WelcomeForm());
-      Application.Run(new TaskForm("FinalJonTest"));
+      Application.Run(new WelcomeForm());
+      //Application.Run(new TaskForm("FinalJonTest"));
       //Application.Run(new MainMenuForm());
       //Application.Run(new SkillSetForm("Jonathan"));
     }
